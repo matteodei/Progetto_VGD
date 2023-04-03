f0b3cb7c6754ba6c6a931db9366eda8f5a756e0e
matteodei <93871655+matteodei@users.noreply.github.com>
2023-04-03 17:07:22 +0200

    restore

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public CharacterController controller;
+    private float speed = 20f;
+    private Vector3 velocity;
+    private float gravity = -9.81f;
+    public float mouseSens = 100f;
+    public Transform cameraTransform;
+    private float rotation = 0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float hzMove = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float vtMove = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        float mouseX = Input.GetAxis("Mouse X") * mouseSens * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSens * Time.deltaTime;
+
+        Vector3 move = transform.right * hzMove + transform.forward * vtMove;
+
+        if (controller.isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+
+        controller.Move(move);
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+        rotation -= mouseY;
+        rotation = Mathf.Clamp(rotation, -90f, 90f);
+
+        cameraTransform.localRotation = Quaternion.Euler(rotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+    }
+}
