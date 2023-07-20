0336a57e402b5809deaf9bac18b2830ebe6e32a8
01Daniel01 <93667461+01Daniel01@users.noreply.github.com>
2023-07-20 21:06:41 +0200

    Codice per raccolta e lancio oggetti (non ancora funzionante)

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,55 @@
+//using System;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public float throwForce;
+    public float throwExtraForce;
+    public float rotationForce;
+
+    public int weaponGfxLayer; //No wall clipping
+    public GameObject weaponGfx;
+    public Collider[] gfxColliders;
+
+    private bool _held;
+    private Rigidbody _rb;
+
+    private void Start()
+    {
+        _rb = gameObject.AddComponent<Rigidbody>();
+        _rb.mass = 1.0f;
+    }
+
+    public void Pickup(Transform weaponHolder)
+    {
+        if (_held) return;
+        Destroy(_rb); //Non interagisce più col mondo una volta raccolta
+        transform.parent = weaponHolder;
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.identity;
+        foreach(var col in gfxColliders) { 
+            col.enabled = false;
+        }
+        _held = true;
+    }
+
+    public void Drop(Transform playerCamera)
+    {
+        if (!_held) return;
+        _rb = gameObject.AddComponent<Rigidbody>();
+        _rb.mass = 1.0f;
+        var forward = playerCamera.forward;
+        forward.y = 0f;
+        _rb.velocity = forward * throwForce;
+        _rb.velocity += Vector3.up * throwExtraForce;
+        _rb.angularVelocity = Random.onUnitSphere * rotationForce;
+        foreach (var col in gfxColliders)
+        {
+            col.enabled = true;
+        }
+        transform.parent = null;
+        _held = false;
+
+    }
+
+}
