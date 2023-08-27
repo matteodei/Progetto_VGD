066efecb4b0a67b2aa154ae5b116e425b6aa58f9
Bacchiu <102520936+Bacchiu@users.noreply.github.com>
2023-08-27 16:51:41 +0200

    Raccolta e posizionamento gemme c

--- Assets/HUD/UsoOggetti.cs
+++ Assets/HUD/UsoOggetti.cs
@@ -6,17 +6,30 @@ using UnityEngine.SceneManagement;
 public class UsoOggetti : MonoBehaviour
 {
     public bool redgem;
+    public bool bluegem;
 
-    public GameObject GemmaRossaObj;
+    
+
+    public GameObject GemmaRossaAlt;
     public GameObject GemmaRossaCan;
-    public GameObject prova;
+    public GameObject GemmaRossa;
+
+    public GameObject GemmaBluAlt;
+    public GameObject GemmaBluCan;
+    public GameObject GemmaBlu;
+
+
+   // public GameObject prova;
     public Collider player;
 
     private void Start()
     {
        
         GemmaRossaCan.SetActive(false);
-        prova.SetActive(false);
+        GemmaRossaAlt.SetActive(false);
+        GemmaBluCan.SetActive(false);
+        GemmaBluAlt.SetActive(false);
+        //prova.SetActive(false);
        
     }
 
@@ -31,31 +44,62 @@ public class UsoOggetti : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Gemma"))
-        {
-
-
-            GemmaRossaObj.SetActive(false);
-            GemmaRossaCan.SetActive(true);
-            redgem = true;
-
-        }
-       
+        
     }
 
     private void OnTriggerStay(Collider other)
     {
-        
+
+        if (other.CompareTag("ZonaGemmaRossa"))
         {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                GemmaRossa.SetActive(false);
+                GemmaRossaCan.SetActive(true);
+                redgem = true;
+
+            }
+        }
+        else if (other.CompareTag("ZonaGemmaBlu"))
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                GemmaBlu.SetActive(false);
+                GemmaBluCan.SetActive(true);
+                bluegem = true;
+
+            }
+        }
+        else if (other.CompareTag("AltareRosso"))
+        {
+            if (Input.GetKeyDown(KeyCode.E) && redgem == true)
+            {
+                GemmaRossaAlt.SetActive(true);
+                GemmaRossaCan.SetActive(false);
+            }
+        }
+        else if (other.CompareTag("AltareBlu"))
+        {
+            if (Input.GetKeyDown(KeyCode.E) && bluegem == true)
+            {
+                GemmaBluAlt.SetActive(true);
+                GemmaBluCan.SetActive(false);
+
+            }
             
-              if (Input.GetKeyDown(KeyCode.C) && redgem == true)
+          
+        }
+
+
+
+           /* if (Input.GetKeyDown(KeyCode.C) && redgem == true)
               { 
                     print("Stai premendo C");
                     prova.SetActive(true);
               
-              }
+              }*/
          
-        }
+        
     }
 
 
